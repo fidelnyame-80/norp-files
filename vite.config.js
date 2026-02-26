4739ea60b08ec8e3d61b2226cf5f3cd41a423d9b
fidelnyame <nyamefidel7@gmail.com>
2026-02-26 14:21:48 +0000

    fixed routes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,20 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        home: `${process.cwd()}/index.html`,
+        categories: `${process.cwd()}/categories/index.html`,
+        categoriesAsian: `${process.cwd()}/categories/asian/index.html`,
+        categoriesBlack: `${process.cwd()}/categories/black/index.html`,
+        categoriesLatina: `${process.cwd()}/categories/latina/index.html`,
+        categoriesWhite: `${process.cwd()}/categories/white/index.html`,
+        categoriesMixed: `${process.cwd()}/categories/mixed/index.html`,
+        popular: `${process.cwd()}/popular/index.html`,
+        search: `${process.cwd()}/search/index.html`,
+        legal2257: `${process.cwd()}/2257/index.html`,
+      },
+    },
+  },
 });
